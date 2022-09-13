3ede8858a4a5eb678d219442ec45a99fa2b64ebd
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-09-13 10:29:37 +0200

    Update insert-video.js

--- javascripts/discourse/initializers/insert-video.js
+++ javascripts/discourse/initializers/insert-video.js
@@ -1,48 +1,47 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
+export const uploadVideo = async (req, res) => {
+  // console.log(req.body, req.file);
 
-  export const uploadVideo = async (req, res) => {
-    // console.log(req.body, req.file);
-
-    const optionsToCreateVideo = {
-      method: "POST",
-      url: `http://video.bunnycdn.com/library/${settings.VIDEO_LIBRARY_ID}/videos`,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        AccessKey: settings.BUNNY_API_KEY,
-      },
-      data: JSON.stringify({ title: req.body.name }),
-    };
-
-    await axios
-    .request(optionsToCreateVideo)
-    .then((response) => {
-      const video_id = response.data.guid;
-
-      axios
-        .put(
-          `http://video.bunnycdn.com/library/${settings.VIDEO_LIBRARY_ID}/videos/${video_id}`,
-          {
-            headers: {
-              AccessKey: settings.BUNNY_API_KEY,
-            },
-            body: req.file ,
-          }
-        )
-        .then(function (response) {
-          res.status(200).json(response);
-        })
-        .catch(function (error) {
-          // console.error("error", error);
-          res.status(400).json(error);
-        });
-    })
-    .catch((error) => {
-      console.log(error.message);
-      res.status(200).json({ error: "failed!" });
-    });
+  const optionsToCreateVideo = {
+    method: "POST",
+    url: `http://video.bunnycdn.com/library/${settings.VIDEO_LIBRARY_ID}/videos`,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      AccessKey: settings.BUNNY_API_KEY,
+    },
+    data: JSON.stringify({ title: file.name }),
   };
+
+  await axios
+  .request(optionsToCreateVideo)
+  .then((response) => {
+    const video_id = response.data.guid;
+
+    axios
+    .put(
+      `http://video.bunnycdn.com/library/${settings.VIDEO_LIBRARY_ID}/videos/${video_id}`,
+      {
+        headers: {
+          AccessKey: settings.BUNNY_API_KEY,
+        },
+        body: file.name,
+      }
+    )
+    .then(function (response) {
+      res.status(200).json(response);
+    })
+    .catch(function (error) {
+      // console.error("error", error);
+      res.status(400).json(error);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    res.status(200).json({ error: "failed!" });
+  });
+};
 
 export default {
   name: "video-compressor",
