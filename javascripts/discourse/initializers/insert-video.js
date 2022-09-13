424e3849cf5ac3c92a935330921004cb435b665c
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-09-13 10:14:41 +0200

    Update insert-video.js

--- javascripts/discourse/initializers/insert-video.js
+++ javascripts/discourse/initializers/insert-video.js
@@ -1,10 +1,50 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-export default {
+  
+export const uploadVideo = async (req, res) => {
   name: "video-compressor",
+  // console.log(req.body, req.file);
 
+  const optionsToCreateVideo = {
+    method: "POST",
+    url: `http://video.bunnycdn.com/library/${settings.VIDEO_LIBRARY_ID}/videos`,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      AccessKey: settings.BUNNY_API_KEY,
+    },
+    data: JSON.stringify({ title: req.body.name }),
+  };
 
-initialize() {
+  await axios
+    .request(optionsToCreateVideo)
+    .then((response) => {
+      const video_id = response.data.guid;
+
+      axios
+        .put(
+          `http://video.bunnycdn.com/library/${settings.VIDEO_LIBRARY_ID}/videos/${video_id}`,
+          {
+            headers: {
+              AccessKey: settings.BUNNY_API_KEY,
+            },
+            body: req.file ,
+          }
+        )
+        .then(function (response) {
+          res.status(200).json(response);
+        })
+        .catch(function (error) {
+          // console.error("error", error);
+          res.status(400).json(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(200).json({ error: "failed!" });
+    });
+
+  initialize() {
     withPluginApi("0.8.31", api => {
       api.addComposerUploadHandler(["mp4", "mov"], (files, editor) => {
         files.forEach((file) => {
