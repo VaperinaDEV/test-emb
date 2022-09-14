cf1f69e4589c2b58871b158388baeecfe4438607
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-09-14 16:50:03 +0200

    Update insert-video.js

--- javascripts/discourse/initializers/insert-video.js
+++ javascripts/discourse/initializers/insert-video.js
@@ -1,31 +1,33 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 function videoCompress() { 
-    var uppy = window.Robodog.pick({
-      waitForEncoding: true,
-      params: {
-        // To avoid tampering, use Signature Authentication
-        auth: { key: settings.TRANSLOADIT_API_KEY },
-        // To hide your `steps`, use a `template_id` instead
-        steps: {
-          webm_encoded: {
-            use: ':original',
-            robot: '/video/encode',
-            result: true,
-            ffmpeg_stack: 'v4.3.1',
-            preset: 'webm',
-            turbo: false
-          }
+  var uppy = window.Robodog.pick({
+    waitForEncoding: true,
+    params: {
+      // To avoid tampering, use Signature Authentication
+      auth: { key: settings.TRANSLOADIT_API_KEY },
+      // To hide your `steps`, use a `template_id` instead
+      steps: {
+        webm_encoded: {
+          use: ':original',
+          robot: '/video/encode',
+          result: true,
+          ffmpeg_stack: 'v4.3.1',
+          preset: 'webm',
+          turbo: false
         }
       }
-    }).then(function (bundle) {
-      // Due to `waitForEncoding: true` this is fired after encoding is done.
-      // Alternatively, set `waitForEncoding` to `false` and provide a `notify_url`
-      // for Async Mode where your back-end receives the encoding results
-      // so that your user can be on their way as soon as the upload completes.
-      console.log(bundle.transloadit) // Array of Assembly Statuses
-      console.log(bundle.results)     // Array of all encoding results
-    }).catch(console.error)
+    }
+  })
+  .then(function (bundle) {
+    // Due to `waitForEncoding: true` this is fired after encoding is done.
+    // Alternatively, set `waitForEncoding` to `false` and provide a `notify_url`
+    // for Async Mode where your back-end receives the encoding results
+    // so that your user can be on their way as soon as the upload completes.
+    console.log(bundle.transloadit) // Array of Assembly Statuses
+    console.log(bundle.results)     // Array of all encoding results
+  })
+  .catch(console.error)
 }
 
 export default {
