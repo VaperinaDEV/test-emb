7655c93e47bc4ddf8c5e2e88015896204920e375
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-09-14 21:41:45 +0200

    Update insert-video.js

--- javascripts/discourse/initializers/insert-video.js
+++ javascripts/discourse/initializers/insert-video.js
@@ -1,42 +1,43 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-function videoCompress() { 
-  var uppy = window.Robodog.pick({
-    waitForEncoding: true,
-    params: {
-      // To avoid tampering, use Signature Authentication
-      auth: { key: settings.TRANSLOADIT_API_KEY },
-      // To hide your `steps`, use a `template_id` instead
-      steps: {
-        ':original': {
-          robot: '/upload/handle'
-        },
-        webm_encoded: {
-          use: ':original',
-          robot: '/video/encode',
-          result: true,
-          ffmpeg_stack: 'v4.3.1',
-          preset: 'webm',
-          turbo: false
-        },
-        exported: {
-          use: ['webm_encoded', ':original'],
-          robot: '/s3/store',
-          credentials: 'YOUR_AWS_CREDENTIALS',
-          url_prefix: 'https://demos.transloadit.com/'
-        }
-      }
+function videoCreate() { 
+  const data = '{"title":"string"}';
+
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
     }
-  })
-  .then(function (bundle) {
-    // Due to `waitForEncoding: true` this is fired after encoding is done.
-    // Alternatively, set `waitForEncoding` to `false` and provide a `notify_url`
-    // for Async Mode where your back-end receives the encoding results
-    // so that your user can be on their way as soon as the upload completes.
-    console.log(bundle.transloadit) // Array of Assembly Statuses
-    console.log(bundle.results)     // Array of all encoding results
-  })
-  .catch(console.error)
+  });
+
+  xhr.open('POST', 'https://video.bunnycdn.com/library/59740/videos');
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.setRequestHeader('Content-Type', 'application/*+json');
+  xhr.setRequestHeader('AccessKey', settings.BUNNY_API_KEY);
+
+  xhr.send(data);
+
+}
+
+function videoUpload() {
+  const data = null;
+
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open('PUT', 'https://video.bunnycdn.com/library/59740/videos/videoId');
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.setRequestHeader('AccessKey', settings.BUNNY_API_KEY);
+
+  xhr.send(data);
 }
 
 export default {
@@ -47,7 +48,8 @@ export default {
       api.addComposerUploadHandler(["mp4", "mov"], (files, editor) => {
         files.forEach((file) => {
           console.log("Handling upload for", file.name);
-          videoCompress();
+          videoCreate();
+          videoUpload();
         });
       });
     });
