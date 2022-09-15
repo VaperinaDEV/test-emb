f5600c6f5a9b3b9b59a136b67d6afa4020dca7d5
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-09-15 15:23:49 +0200

    Update insert-video.js

--- javascripts/discourse/initializers/insert-video.js
+++ javascripts/discourse/initializers/insert-video.js
@@ -53,7 +53,7 @@ export default {
 
   initialize() {
     withPluginApi("0.8.31", api => {
-      var button = $('.discourse-video-upload-modal label.btn')
+      var button = $('.discourse-video-upload-modal input[type=file]')
       button.on('click', function() {
         checkFileDuration();
       });
